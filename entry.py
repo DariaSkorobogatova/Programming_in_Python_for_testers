15bdb5c0e2ab600877e4018232381b2d580ec21f
Daria Skorobogatova <daria.a.skorobogatova@gmail.com>
2022-06-21 13:33:21 +0300

    passed parameters into methods, created class Entry

---
+++ entry.py
@@ -0,0 +1,14 @@
+class Entry:
+    def __init__(self,firstname, middlename, lastname, nickname, title, company, address, mobile, bday, bmonth,
+                     byear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
--- test_add_new_entry.py
+++ test_add_new_entry.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from entry import Entry
 
 class TestAddNewEntry(unittest.TestCase):
     def setUp(self):
@@ -13,9 +14,11 @@ class TestAddNewEntry(unittest.TestCase):
     def test_add_new_entry(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.init_entry_creation(wd)
-        self.create_entry(wd)
+        self.create_entry(wd,  Entry(firstname="Freddie", middlename="Queen", lastname="Mercury", nickname="Queen",
+        title="Singer", company="Queen", address="London, England", mobile="8888888888", bday="5",
+        bmonth="September", byear="1946"))
         self.return_to_home_page(wd)
         self.logout(wd)
 
@@ -25,41 +28,41 @@ class TestAddNewEntry(unittest.TestCase):
     def return_to_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
 
-    def create_entry(self, wd):
+    def create_entry(self, wd, entry):
         # fill entry's fields
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Freddie")
+        wd.find_element_by_name("firstname").send_keys(entry.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Queen")
+        wd.find_element_by_name("middlename").send_keys(entry.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Mercury")
+        wd.find_element_by_name("lastname").send_keys(entry.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("Queen")
+        wd.find_element_by_name("nickname").send_keys(entry.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("Singer")
+        wd.find_element_by_name("title").send_keys(entry.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("Queen")
+        wd.find_element_by_name("company").send_keys(entry.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("London, England")
+        wd.find_element_by_name("address").send_keys(entry.address)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("8888888888")
+        wd.find_element_by_name("mobile").send_keys(entry.mobile)
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("5")
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(entry.bday)
         wd.find_element_by_xpath("//option[@value='5']").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("September")
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(entry.bmonth)
         wd.find_element_by_xpath("//option[@value='September']").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1946")
+        wd.find_element_by_name("byear").send_keys(entry.byear)
         # submit entry creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
@@ -69,12 +72,12 @@ class TestAddNewEntry(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("https://localhost/addressbook/")
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def is_element_present(self, how, what):
