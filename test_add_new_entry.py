515035bb7700daefb06348fb1188ab45a59b0d81
Daria Skorobogatova <daria.a.skorobogatova@gmail.com>
2022-06-25 20:38:47 +0300

    segregated a fixture, transferred all the auxiliary functions to it, got rid of test class using pytest

--- test_add_new_entry.py
+++ test_add_new_entry.py
@@ -1,98 +1,29 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from entry import Entry
+from application import Application
 
-class TestAddNewEntry(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-    
-    def test_add_new_entry(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.init_entry_creation(wd)
-        self.create_entry(wd,  Entry(firstname="Freddie", middlename="Queen", lastname="Mercury", nickname="Queen",
-        title="Singer", company="Queen", address="London, England", mobile="8888888888", bday="5",
-        bmonth="September", byear="1946"))
-        self.return_to_home_page(wd)
-        self.logout(wd)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-    def create_entry(self, wd, entry):
-        # fill entry's fields
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(entry.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(entry.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(entry.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(entry.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(entry.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(entry.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(entry.address)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(entry.mobile)
-        wd.find_element_by_name("bday").click()
-        #Select(wd.find_element_by_name("bday")).select_by_visible_text(entry.bday)
-        wd.find_element_by_xpath("//option[@value='" + entry.bday + "']").click()
-        wd.find_element_by_name("bmonth").click()
-        #Select(wd.find_element_by_name("bmonth")).select_by_visible_text(entry.bmonth)
-        wd.find_element_by_xpath("//option[@value='" + entry.bmonth + "']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(entry.byear)
-        # submit entry creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def init_entry_creation(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def open_home_page(self, wd):
-        wd.get("https://localhost/addressbook/")
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def tearDown(self):
-        self.wd.quit()
+def test_add_new_entry(app):
+    app.open_home_page()
+    app.login(username="admin", password="secret")
+    app.init_entry_creation()
+    app.create_entry(Entry(firstname="Freddie", middlename="Queen", lastname="Mercury", nickname="Queen",
+    title="Singer", company="Queen", address="London, England", mobile="8888888888", bday="5",
+    bmonth="September", byear="1946"))
+    app.return_to_home_page()
+    app.logout()
 
 
-if __name__ == "__main__":
-    unittest.main()
+
+
+
+
+
+
+
